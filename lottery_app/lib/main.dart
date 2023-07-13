5506428b2ff4d44277048a16e204d6cea04596ca
AroojAslam <101873027+AroojAslam@users.noreply.github.com>
2023-07-13 12:06:50 +0500

    create project

---
+++ lottery_app/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LotteryApp());
+}
+
+class LotteryApp extends StatefulWidget {
+  const LotteryApp({super.key});
+
+  @override
+  State<LotteryApp> createState() => _LotteryAppState();
+}
+
+class _LotteryAppState extends State<LotteryApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
